2fdcd6081500e6304ffd37d189595cd6a9178c80
stedyyulius <stedyyulius@gmail.com>
2017-03-29 16:10:17 +0700

    first commit

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,44 @@
 const readline = require('readline');
-// your code here to initialize the program and take user input
+rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: 'Please input text here =>'
+});
 
-module.exports = Dictionary
+rl.prompt();
+
+var box = [];
+function dictionarySort(Input){
+  for(var i = 1; i < box.length; i++) {
+    for(var j = 0; j < i; j++) {
+      if(box[i].toLowerCase() < box[j].toLowerCase()) {
+        var lower = box[i];
+        box[i] = box[j];
+        box[j] = lower;
+      }
+    }
+  }
+  for(var i = 0; i < box.length; i++) {
+    console.log(` ${box[i]}`);
+  }
+}
+
+function container(){
+  rl.on('line', (userInput) => {
+ if (userInput.length === 0) {
+      rl.close();
+    } else {
+      console.log(`${userInput} added!`)
+      rl.setPrompt('> Type another word (or press enter to finish): ');
+      rl.prompt();
+      box.push(userInput);
+    }
+
+  }).on('close', () => {
+    console.log(`> Congratulations! Your dictionary has ${box.length} words:`)
+    dictionarySort(box);
+  });
+}
+container();
+
+module.exports = dictionarySort
