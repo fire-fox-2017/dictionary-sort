d708ed9d6c551652fac0c3d67695bd7f958a84af
fearzen3 <ivanhabi2@gmail.com>
2017-03-29 21:38:50 +0700

    selse

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,47 @@
 const readline = require('readline');
-// your code here to initialize the program and take user input
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: 'OHAI> '
+});
 
-module.exports = Dictionary
+let data=[];
+
+
+
+function nonSensitive(s1, s2) {
+  var s1lower = s1.toLowerCase();
+  var s2lower = s2.toLowerCase();
+
+	if (s1lower > s2lower){
+		return 1;
+}else if(s1lower < s2lower){
+		return -1;
+}else{
+		return 0;
+}
+}
+
+
+rl.prompt();
+
+rl.on('line', (line) => {
+  switch(line.trim()) {
+    case '':
+      console.log('angka kosong');
+      break;
+    default:
+
+
+data.push(line);
+console.log(data.sort(nonSensitive));
+
+      break;
+  }
+  rl.prompt();
+}).on('close', () => {
+  console.log('Have a great day!');
+  process.exit(0);
+});
+
+//module.exports = Dictionary
