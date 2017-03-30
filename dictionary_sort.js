800873afaad67b229cce377adfd7b17443f35a89
Parel Wellman Hutahaean <parel.hutahaean@gmail.com>
2017-03-31 02:14:38 +1400

    Second Commit to Dictionary Sort

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -27,7 +27,7 @@ rl.on("line", input => {
 const Dictionary = arr => {
   for (let i = 0; i < arr.length - 1; i++) {
     for (let j = i + 1; j < arr.length; j++){
-      if (arr[j] < arr[i]) {
+      if (arr[j].toLowerCase() < arr[i].toLowerCase()) {
         let temp = arr[j];
         arr[j] = arr[i];
         arr[i] = temp;
