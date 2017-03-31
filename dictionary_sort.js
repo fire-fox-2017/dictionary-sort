89334cd5eed51387a16b9b534defcc023452e82c
haryanawisnu <arya.wisnu05@gmail.com>
2017-03-31 15:46:15 +0700

    done

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -15,33 +15,11 @@ rl.on('line', (aksi) => {
 	rl.prompt();
 })
 function dictionary(arrIn){
-  arrHuruf=["","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-  arrTemp=[];
-	for(var i=0;i<arrIn.length;i++){
-		var temp=arrIn[i];
-        temp=temp.charAt(0);
-		for(var j=0;j<arrHuruf.length;j++){
-			if(temp==arrHuruf[j]){
-				arrTemp.push(j+100);
-			}else if(temp.toUpperCase()==arrHuruf[j]){
-				arrTemp.push(j);
-            }
-		}
+    arrIn.sort(function(a,b){
+        return a.toLowerCase().localeCompare(b.toLowerCase());
+    })
+    return arrIn;
 	}
-    for(var k= 1;k<arrIn.length;k++){
-      for(var l=k;l>0;l--){
-        if(arrTemp[l]< arrTemp[l-1]){
-          var temp1 = arrTemp[l];
-            var temp2 = arrIn[l];
-          arrTemp[l] = arrTemp[l-1];
-          arrTemp[l-1] = temp1;
-            arrIn[l] = arrIn[l-1];
-            arrIn[l-1] = temp2;
-		      }
-        }
-	}
-  return arrIn;
-}
 rl.on('close', () => {
 	console.log("");
 	console.log("Congratulations! Your dictionary has "+tempArr.length+" words : ");
