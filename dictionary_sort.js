f05ad50ddbea38c214035344161a748c9ab2624f
dyankastutara <dyankastutara19@gmail.com>
2017-03-31 09:53:55 +0700

    dyankastutara done

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,41 @@
 const readline = require('readline');
 // your code here to initialize the program and take user input
+const rl = readline.createInterface({
+  input:process.stdin,
+  output:process.stdout,
+  prompt:"Type a Word:\n"
+});
+rl.prompt();
+var arr=[];
+rl.on('line',(input)=>{
+  if(input === ""){
+    console.log("Congratulations! Your dictionary has "+arr.length+" words");
+    Dictionary();
+    rl.close();
+  }else{
+  arr.push(input);
+  console.log("Type another word (or press enter to finish):");
+  }
+});
 
-module.exports = Dictionary
+function Dictionary(){
+  var sorting = Sort(arr);
+  for (let i = 0; i < sorting.length; i++) {
+    console.log(sorting[i]);
+  }
+}
+
+const Sort = arr =>{
+  for(let i=0;i<arr.length-1;i++){
+    for(let j=i+1;j<arr.length;j++){
+      if(arr[j].toLowerCase() < arr[i].toLowerCase()){
+        var temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+//module.exports = Dictionary
